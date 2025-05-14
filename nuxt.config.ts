be253c9aa7e89d51856b625458a2f08909efec34
somoschispudos <somos@chispudos.com>
2025-05-13 20:08:34 -0600

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	modules: ["@nuxt/eslint"],
+	devtools: { enabled: true },
+	compatibilityDate: "2024-11-01",
+	css: ["@picocss/pico"],
+	eslint: {
+		config: {
+			stylistic: {
+				semi: true,
+				quotes: "double",
+				commaDangle: "always-multiline",
+				indent: "tab",
+			},
+		},
+	},
+});
